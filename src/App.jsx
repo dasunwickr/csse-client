02295d15604f713177d048660270f264c20970b7
dasunwickr <sathsarawd@gmail.com>
2024-10-19 12:37:12 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WasteCollectorProfile from "./views/collector/WasteCollectorProfile";
-import Map from "./components/collector/Map"; // Add other components if needed
 import WMA from "./components/wma/WMA";
 import Admin from "./components/admin/Admin";
 import Resident from "./components/resident/Resident";
@@ -11,18 +10,12 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Define routes here */}
-
-        <Route path="wma/" element={<WMA />} />
-
-        <Route path="admin/" element={<Admin />} />
-
-        <Route path="resident/" element={<Resident />} />
-
-        <Route path="collector/" element={<Contoller />} />
-        <Route path="collector/home" element={<WasteCollectorProfile />} />
-
-        {/* Add more routes as needed */}
+        <Route path="/wma" element={<WMA />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/resident" element={<Resident />} />
+        <Route path="/collector" element={<Contoller />} >
+          <Route path="/home" element={<WasteCollectorProfile />} />
+        </Route>
       </Routes>
     </Router>
   );
