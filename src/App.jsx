2a9ed9b9e24dd4140ec60ac47cd820865c7d0776
dasunwickr <sathsarawd@gmail.com>
2024-10-19 12:25:55 +0530

    Intial Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Button from "@mui/material/Button";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
